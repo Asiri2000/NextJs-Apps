06dad94762f6448747b63dafcf52bfe47ce3d2f3
Asiri2000 <149108334+Asiri2000@users.noreply.github.com>
2026-05-25 08:46:31 +0530

    added localization

---
+++ my-app/src/app/translate/home/page.tsx
@@ -0,0 +1,22 @@
+
+import {useTranslations} from 'next-intl';
+
+export default function Home (){
+
+    const t = useTranslations('Homepage');
+
+    return(
+
+
+
+
+        <>
+        
+          <h1>{t('title')}</h1>
+          <p>{t('content')}</p>
+        </>
+
+    )
+
+
+}
